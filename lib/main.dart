f37383ea5a000fbd6d2e9edf63ac3cdc9a84aead
Mahmouuddiab <diabmahmoud450@gmail.com>
2026-09-21 16:12:57 +0300

    Fix counter reset bug

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
         useMaterial3: true,
       ),
+      debugShowCheckedModeBanner: false,
       home: const CounterPage(title: 'CI/CD Demo Counter'),
     );
   }
